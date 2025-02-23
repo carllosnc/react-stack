078b8650aa4a71f66d7d0e3e49c62bafd76fd4b2
Carlos Costa <carllos.nc@gmail.com>
2025-02-23 12:26:05 -0300

    Add page creation functionality to Dashboard with loading state and error handling

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,6 +2,10 @@ import type { Route } from "../+types/root";
 import { getAuth } from "@clerk/react-router/ssr.server"
 import { redirect } from "react-router"
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
+import { Spinner } from "@/components/ui/spinner";
+import { useUser } from "@clerk/react-router";
+import { Button } from "@/components/ui/button";
+import { useCreatePage } from "@/app/db-hooks/page-hooks";
 
 export async function loader(args: any) {
   const auth = await getAuth(args)
@@ -18,9 +22,37 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Dashboard() {
+  const { user, isLoaded } = useUser()
+  const { mutate, isPending, data, isError, error } = useCreatePage()
+
+  if (!isLoaded) {
+    return <div>
+      <Spinner />
+    </div>
+  }
+
+  function handleCreatePage() {
+    mutate({
+      title: "New page 2",
+      description: "This is a new page 2",
+      slug: "new-page2",
+      user_id: user?.id,
+      is_public: 1,
+    })
+  }
+
+  if (isError) {
+    console.log(error.message)
+  }
+
   return (
     <div>
       <DashboardHeader />
+      <div className="p-[20px]">
+        <Button onClick={handleCreatePage}>
+          Add page
+        </Button>
+      </div>
     </div>
   );
 }
