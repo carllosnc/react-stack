375f57b8c85537a384b4c4105b971d182a89a783
Carlos Costa <carllos.nc@gmail.com>
2025-02-23 12:25:58 -0300

    Refactor BaseLayout component to use <main> instead of <div> for improved semantic structure

--- app/layouts/base.tsx
+++ app/layouts/base.tsx
@@ -2,10 +2,8 @@ import { Outlet } from "react-router";
 
 export default function BaseLayout() {
   return (
-    <div className="bg-neutral-100 w-full min-h-screen">
-      <div>
-        <Outlet />
-      </div>
-    </div>
+    <main className="bg-neutral-100 w-full min-h-screen">
+      <Outlet />
+    </main>
   );
 }
