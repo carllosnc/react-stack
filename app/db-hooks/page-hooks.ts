52d3004561dbd003ff1ba11dbb02dd7ba78b765e
Carlos Costa <carllos.nc@gmail.com>
2025-02-23 12:25:54 -0300

    Add custom hooks for page management using React Query

---
+++ app/db-hooks/page-hooks.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getPages, createPage } from "@/app/repository/page-repository";
+import type { NewPage } from "@/lib/db-types";
+
+function useGetPages(userId: string) {
+    return useQuery({
+      queryKey: ["getPages", userId],
+      queryFn: async () => {
+        const pages = await getPages(userId)
+        return pages
+      }
+  })
+}
+
+function useCreatePage(){
+  return useMutation({
+    mutationFn: async (data: NewPage) => {
+      const page = await createPage(data)
+
+      return page
+    }
+  })
+}
+
+export {
+  useGetPages,
+  useCreatePage
+}
